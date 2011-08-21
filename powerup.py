f71337c99780754e031d06b13cceef455a29fc52
ktf <not.discostoo@gmail.com>
2011-08-20 22:49:20 -0700

    Powerup now inherits from Enemy

--- powerup.py
+++ powerup.py
@@ -1,6 +1,8 @@
 import random
 import pygame
 
+from enemy import Enemy
+
 # TODO: doesn't work for some reason
 #from blobulous import SCREEN_W, SCREEN_H
 SCREEN_W = 800
@@ -9,26 +11,19 @@ SCREEN_H = 600
 POWERUP_SIZE = 12
 POWERUP_COLOR = (0, 255, 0)
 
-# TODO: should this just inherit from Enemy? They share a lotta code...
-class Powerup(pygame.sprite.Sprite):
 
-    # -- Attributes
-
-    # Set speed vector
-    change_x = 0
-    change_y = 0
-
-    # Whether this powerup is currently targeted by the player
-    targeted = False
-     
-    # -- Methods
+# Inherits from Enemy class. That doesn't quite make sense, conceptually. They
+# should probably both inherit from a TargetableObject class or something, but
+# this works for now.
+class Powerup(Enemy):
 
     # TODO: not sure if I have to set default values for x and y, but I want to
     # be able to just go Powerup(randomize=True) without passing in x and y
     # values, so default values seem necessary.
-    def __init__(self, x=0, y=0, randomize=False):
-        # Call the parent's constructor
-        pygame.sprite.Sprite.__init__(self)
+    def __init__(self, x=0, y=0, dx=0, dy=0, randomize=False):
+
+        # Call the parent's constructor (which calls Sprite's constructor...)
+        Enemy.__init__(self, x, y, dx, dy, randomize)
          
         # Set height, width
         self.image = pygame.Surface([POWERUP_SIZE, POWERUP_SIZE])
@@ -39,17 +34,17 @@ class Powerup(pygame.sprite.Sprite):
         
         if randomize == False:
             self.rect.topleft = [x, y]
+            self.delta_x = dx
+            self.delta_y = dy
         else:
+            # Random initial position
             rand_x = random.randint(0, SCREEN_W - POWERUP_SIZE)
             rand_y = random.randint(0, SCREEN_H - POWERUP_SIZE)
             self.rect.topleft = [rand_x, rand_y]
+
+            # Random initial speed and direction
+            self.delta_x = random.randint(-5, 6)
+            self.delta_y = random.randint(-5, 6)
      
-    # Change the speed of the powerup
-    def changespeed(self, x, y):
-        self.change_x += x
-        self.change_y += y
-         
-    # Find a new position for the powerup
-    def update(self):
-        self.rect.top += self.change_y
-        self.rect.left += self.change_x
+def spawn_powerup():
+    pass
