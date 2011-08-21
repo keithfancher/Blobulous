1c05b7208c475c43bfb40b1d8fcec80bcc6822fb
ktf <not.discostoo@gmail.com>
2011-08-20 21:28:44 -0700

    oh hello

---
+++ blobulous.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+
+import sys
+
+import pygame
+
+from player import Player
+from enemy import Enemy
+ 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+
+# Screen size
+SCREEN_W = 800
+SCREEN_H = 600
+
+# Random constants for testing
+NUM_ENEMIES = 5
+FPS = 30
+
+ 
+# PyGame init stuff
+pygame.init()
+screen = pygame.display.set_mode([SCREEN_W, SCREEN_H])
+pygame.display.set_caption('Blobulous')
+clock = pygame.time.Clock()
+ 
+# Create the player object
+player = Player(SCREEN_W / 2, SCREEN_H / 2)
+
+# Does this really need its own Sprite Group?
+player_group = pygame.sprite.RenderPlain((player))
+
+# Create NUM_ENEMIES random enemies
+enemies = pygame.sprite.RenderPlain()
+for i in range(0, NUM_ENEMIES):
+    enemies.add(Enemy(randomize=True))
+
+# Mouse state... need to know if the button is held down or not, not just when
+# it's pressed or released. Maybe PyGame has a better way to do this?
+mouse_down = False
+ 
+# Main event loop
+while True:
+     
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print "Shutting everything down..."
+            sys.exit(0)
+ 
+        # TODO: non-shitty keyboard controls... and joystick?
+        # Set the speed based on the key pressed
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                player.changespeed(-3,0)
+            if event.key == pygame.K_RIGHT:
+                player.changespeed(3,0)
+            if event.key == pygame.K_UP:
+                player.changespeed(0,-3)
+            if event.key == pygame.K_DOWN:
+                player.changespeed(0,3)
+                 
+        # Reset speed when key goes up      
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                player.changespeed(3,0)
+            if event.key == pygame.K_RIGHT:
+                player.changespeed(-3,0)
+            if event.key == pygame.K_UP:
+                player.changespeed(0,3)
+            if event.key == pygame.K_DOWN:
+                player.changespeed(0,-3)
+
+        # Might do something with this later, but not now...
+        if event.type == pygame.MOUSEMOTION:
+            pass
+
+        # When the mouse button is released, kill any targeted enemies
+        if event.type == pygame.MOUSEBUTTONUP:
+            mouse_down = False
+            player.kill_targeted()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_down = True
+
+    # Have to check if we're over an enemy with the mouse every frame even when
+    # there isn't a MOUSEMOTION event, 'cause the player could be holding down
+    # the button and an enemy could move under the mouse.
+    #
+    # This seems pretty inefficient, there's probably a better way to do this.
+    if mouse_down:
+        mouse_position = pygame.mouse.get_pos()
+        for enemy in enemies:
+            if enemy.rect.collidepoint(mouse_position):
+                player.target_enemy(enemy)
+                 
+    # This actually moves the player block based on the current speed
+    player.update()
+     
+    # -- Draw everything
+    # Clear screen
+    screen.fill(BLACK)
+
+    # Draw target lines
+    player.draw_target_lines(screen)
+     
+    # Draw player
+    player_group.draw(screen)
+    
+    # Draw enemies
+    enemies.draw(screen)
+     
+    # Flip screen
+    pygame.display.flip()
+     
+    # Pause
+    clock.tick(FPS)
---
+++ player.py
@@ -0,0 +1,72 @@
+import pygame
+
+PLAYER_SIZE = 10
+PLAYER_COLOR = (0, 0, 255)
+TARGET_LINE_COLOR = (255, 0, 0)
+
+class Player(pygame.sprite.Sprite):
+ 
+    # -- Attributes
+
+    # Set speed vector
+    change_x = 0
+    change_y = 0
+
+    # all the enemies the player is targeting
+    targeted = pygame.sprite.Group()
+
+    # Max targets can have at once... this will increase w/ powerups
+    max_targets = 3;
+     
+    # -- Methods
+
+    def __init__(self, x, y):
+        # Call the parent's constructor
+        pygame.sprite.Sprite.__init__(self)
+         
+        # Set height, width
+        self.image = pygame.Surface([PLAYER_SIZE, PLAYER_SIZE])
+        self.image.fill((PLAYER_COLOR))
+ 
+        # Make our top-left corner the passed-in location.
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x, y]
+     
+    # Change the speed of the player
+    def changespeed(self, x, y):
+        self.change_x += x
+        self.change_y += y
+         
+    # Find a new position for the player
+    def update(self):
+        self.rect.top += self.change_y
+        self.rect.left += self.change_x
+
+    # Add an enemy to the targeted group
+    def target_enemy(self, enemy):
+        # don't target if we've reached our max
+        if len(self.targeted) < self.max_targets:
+            self.targeted.add(enemy)
+            enemy.targeted = True
+
+    # Remove enemy from the targeted group
+    def untarget_enemy(self, enemy):
+        self.targeted.remove(enemy)
+        enemy.targeted = False
+
+    # Remove all enemies from the targted group 
+    def untarget_all(self):
+        self.targeted.empty()
+
+    # Kills all targeted enemies
+    def kill_targeted(self):
+        for target in self.targeted:
+            target.kill()
+        
+    # Draw the lines from self to targets
+    def draw_target_lines(self, surface):
+        # if there are sprites in the targeted group
+        if(self.targeted):
+            for target in self.targeted:
+                # target from and to the *middle* of the objects
+                pygame.draw.line(surface, TARGET_LINE_COLOR, self.rect.center, (target.rect.center))
