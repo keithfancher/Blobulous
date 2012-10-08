a10d959a9d985b32ab0414b1744d39bdd5f709b4
ktf <not.discostoo@gmail.com>
2012-10-07 19:49:07 -0700

    add license

--- settings.py
+++ settings.py
@@ -1,4 +1,23 @@
+# Copyright 2011, 2012 Keith Fancher
+#
+# This file is part of Blobulous.
+#
+# Blobulous is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Blobulous is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Blobulous.  If not, see <http://www.gnu.org/licenses/>.
+
+
 """A bunch of constants for testing and tweaking settings"""
+
 
 # Screen size
 #SCREEN_W = 800
